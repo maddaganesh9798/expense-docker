f5642963c1af1820ef30dfe74759d6a8d71e03c5
maddaganesh9798 <madda.ganesh9798@gmail.com>
2026-04-05 10:27:32 +0530

    docker

--- installdocker.sh
+++ installdocker.sh
@@ -5,7 +5,7 @@ N="\e[0m"
 
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
